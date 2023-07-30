b2e43adfdc9985dff83c2668196ee3758c746c39
SIVA <139126748+Sivaramanguluri@users.noreply.github.com>
2023-07-30 17:22:34 +0530

    Add files via upload

---
+++ PYTHON BASIC PROGRAMS/square of number.py
@@ -0,0 +1,41 @@
+#1
+a = 3
+square = a * a
+print(square)
+
+
+#2 by defining a function
+
+def square(a):
+    return a*a
+
+
+print(square(5))
+
+
+#3 Print the Square of a Number in Python Using the exponent function
+n = 10
+result = n ** 2
+print(result)
+
+
+#4 by taking input from the end-user
+
+n = int(input("Enter a number: "))
+result = n ** 2
+print(result)
+
+
+#5 Print the Square of a Number in Python Using the pow() method
+n = 11
+square = pow(n, 2)
+print(square)
+
+
+
+#6 program by taking input from the end-user.
+
+n = int(input("Enter a number: "))
+square = pow(n, 2)
+print(square)
+
